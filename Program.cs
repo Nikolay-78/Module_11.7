f76b8c7448b7168f839e6e55e0793c08e715d386
Nikolay-78 <kalyam@mail.ru>
2020-11-26 23:06:33 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Module_11._7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter your name: ");
+            string name = Console.ReadLine();
+            Console.Write("Enter your age: ");
+            string age = Console.ReadLine();
+            Console.WriteLine("Your name is {0} and age is {1}", name, age);
+            Console.Write("Enter your birthdate: ");
+            var birthdate = Console.ReadLine();
+            Console.WriteLine("Your birthdate is " + birthdate);
+            Console.ReadKey();
+        }
+    }
+}
